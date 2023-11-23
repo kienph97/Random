3d6053a1ac368128aebb3e8b4317f9d28f1f1b7b
kienph97 <phamtrungkien1281997@gmail.com>
2023-11-24 00:10:32 +0700

    Add icon app

--- app/src/main/java/com/example/random/MainActivity.kt
+++ app/src/main/java/com/example/random/MainActivity.kt
@@ -12,28 +12,12 @@ import com.example.random.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
-    var mService: IMyAidlInterface? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
-        val intentService = Intent(this, AidlService::class.java)
-        bindService(intentService, mCollection, Context.BIND_AUTO_CREATE)
-        startService(intentService)
-//        mBinding.btn.setOnClickListener {
-//            val a = mService?.receiveData()
-//            Log.d("kien", "$a")
-//        }
-    }
-
-    val mCollection = object : ServiceConnection {
-        override fun onServiceConnected(p0: ComponentName?, service: IBinder?) {
-            mService = IMyAidlInterface.Stub.asInterface(service)
-        }
-
-        override fun onServiceDisconnected(p0: ComponentName?) {
-
-        }
 
     }
+
+
 }
