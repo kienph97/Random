7badb4e0df0aa6551dc907a3ef8ac8a5d93fb117
kienph97 <phamtrungkien1281997@gmail.com>
2024-02-14 22:01:28 +0700

    show grill or list in home

--- app/src/main/java/com/kien/random/presenter/adapters/HomeAdapter.kt
+++ app/src/main/java/com/kien/random/presenter/adapters/HomeAdapter.kt
@@ -2,18 +2,27 @@ package com.kien.random.presenter.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import com.kien.random.databinding.LayoutItemHomeBinding
 import com.kien.random.entities.HomeModel
 
 class HomeAdapter: RecyclerView.Adapter<com.kien.random.presenter.adapters.HomeAdapter.HomeViewHolder>() {
     private var mListData = arrayListOf<com.kien.random.entities.HomeModel>()
+    private var mTypeDisplay = LIST
     var mCallBackClickItem: ((Int) -> Unit)? = null
 
     fun setData(list: ArrayList<com.kien.random.entities.HomeModel>) {
         mListData.clear()
         mListData.addAll(list)
+        notifyDataSetChanged()
     }
+
+    fun setTypeDisplay(type: String) {
+        mTypeDisplay = type
+    }
+
+    fun getTypeDisplay() = mTypeDisplay
 
     fun setCallBackClickItem(callback: (Int) -> Unit) {
         mCallBackClickItem = callback
@@ -21,15 +30,35 @@ class HomeAdapter: RecyclerView.Adapter<com.kien.random.presenter.adapters.HomeA
 
     inner class HomeViewHolder(private val itemHomeBinding: LayoutItemHomeBinding): RecyclerView.ViewHolder(itemHomeBinding.root) {
         fun bindData(item: com.kien.random.entities.HomeModel) {
-            itemHomeBinding.apply {
-                imvIcon.setImageResource(item.icon)
-                tvTitle.text = item.title
-                root.apply {
-                    setOnClickListener {
-                        mCallBackClickItem?.invoke(item.id)
+            when (mTypeDisplay) {
+                GRILL -> {
+                    itemHomeBinding.apply {
+                        ctlList.isVisible = false
+                        ctlGrill.isVisible = true
+                        imvIconGrill.setImageResource(item.icon)
+                        tvTitleGrill.text = item.title
+                        root.apply {
+                            setOnClickListener {
+                                mCallBackClickItem?.invoke(item.id)
+                            }
+                        }
+                    }
+                }
+                LIST -> {
+                    itemHomeBinding.apply {
+                        ctlList.isVisible = true
+                        ctlGrill.isVisible = false
+                        imvIcon.setImageResource(item.icon)
+                        tvTitle.text = item.title
+                        root.apply {
+                            setOnClickListener {
+                                mCallBackClickItem?.invoke(item.id)
+                            }
+                        }
                     }
                 }
             }
+
         }
     }
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeAdapter.HomeViewHolder {
@@ -42,4 +71,9 @@ class HomeAdapter: RecyclerView.Adapter<com.kien.random.presenter.adapters.HomeA
     }
 
     override fun getItemCount() = mListData.size
+
+    companion object {
+        const val GRILL = "GRILL"
+        const val LIST = "LIST"
+    }
 }
--- app/src/main/java/com/kien/random/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/kien/random/ui/fragment/HomeFragment.kt
@@ -1,6 +1,8 @@
 package com.kien.random.ui.fragment
 
+import android.content.Context.MODE_PRIVATE
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import android.os.Bundle
 import android.transition.Slide
@@ -11,13 +13,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.kien.random.BuildConfig
 import com.kien.random.R
 import com.kien.random.databinding.FragmentHomeBinding
 import com.kien.random.presenter.adapters.HomeAdapter
+import com.kien.random.presenter.adapters.HomeAdapter.Companion.GRILL
+import com.kien.random.presenter.adapters.HomeAdapter.Companion.LIST
 
 class HomeFragment : BaseFragment(), View.OnClickListener {
+    private var mSharedPreferences: SharedPreferences? = null
+    private var mEditor: SharedPreferences.Editor? = null
     private var mBinding: FragmentHomeBinding? = null
     private val mHomeAdapter by lazy { HomeAdapter() }
 
@@ -30,9 +37,21 @@ class HomeFragment : BaseFragment(), View.OnClickListener {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        initSharedPreferences()
         initView()
         initListener()
         return mBinding?.root
+    }
+
+    private fun initSharedPreferences() {
+        mSharedPreferences = context?.getSharedPreferences(KEY_SHOW, MODE_PRIVATE)
+        mEditor = mSharedPreferences?.edit()
+        if (mSharedPreferences?.getString(KEY_SHOW, "").isNullOrEmpty()) {
+            mEditor?.apply {
+                putString(KEY_SHOW, LIST)
+                apply()
+            }
+        }
     }
 
     private fun initListener() {
@@ -42,17 +61,18 @@ class HomeFragment : BaseFragment(), View.OnClickListener {
         mBinding?.rate?.setOnClickListener(this)
         mBinding?.share?.setOnClickListener(this)
         mBinding?.privacyPolicy?.setOnClickListener(this)
+        mBinding?.imvShowItem?.setOnClickListener(this)
     }
 
     private fun initView() {
         context?.let { com.kien.random.entities.HomeModel().getListItem(it) }?.let { mHomeAdapter.setData(it) }
         mBinding?.rcvItemHome?.apply {
             adapter = mHomeAdapter
-            layoutManager = LinearLayoutManager(
+            layoutManager = if (mSharedPreferences?.getString(KEY_SHOW, "") == LIST) LinearLayoutManager(
                 context,
                 LinearLayoutManager.VERTICAL,
                 false
-            )
+            ) else GridLayoutManager(context, 2)
         }
 
         mHomeAdapter.setCallBackClickItem {
@@ -126,11 +146,42 @@ class HomeFragment : BaseFragment(), View.OnClickListener {
                 intent.data = Uri.parse(URL)
                 startActivity(intent)
             }
+            R.id.imvShowItem -> {
+                if (mSharedPreferences?.getString(KEY_SHOW, "") == GRILL) {
+                    mBinding?.imvShowItem?.setImageResource(R.drawable.ic_grill_item)
+                    mEditor?.apply {
+                        putString(KEY_SHOW, LIST)
+                        apply()
+                    }
+                } else {
+                    mBinding?.imvShowItem?.setImageResource(R.drawable.ic_list_item)
+                    mEditor?.apply {
+                        putString(KEY_SHOW, GRILL)
+                        apply()
+                    }
+                }
+                mHomeAdapter.apply {
+                    setTypeDisplay(
+                        if (mHomeAdapter.getTypeDisplay() == GRILL) {
+                            LIST
+                        } else {
+                            GRILL
+                        }
+                    )
+                }
+                mBinding?.rcvItemHome?.layoutManager = if (mHomeAdapter.getTypeDisplay() == GRILL) {
+                    GridLayoutManager(context, 2)
+                } else {
+                    LinearLayoutManager(context)
+                }
+                context?.let { com.kien.random.entities.HomeModel().getListItem(it) }?.let { mHomeAdapter.setData(it) }
+            }
         }
 
     }
     companion object {
         const val URL = "https://www.freeprivacypolicy.com/live/d5983e33-6ac4-407d-b72f-1b017aa4bf17"
         const val URL_APP = "https://play.google.com/store/apps/details?id=com.kien.random"
+        const val KEY_SHOW = "KEY_SHOW"
     }
 }
